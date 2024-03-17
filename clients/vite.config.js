f0610d22c3803f705251c6c080fdd868766d5524
Jakob Schwendner <jschwendner@krakenrobotics.com>
2024-03-17 22:46:45 +0100

    Add WebSocket proxy configuration

--- clients/vite.config.js
+++ clients/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/ws': {
+        target: 'http://localhost:4538/',
+        ws: true,
+      },
+    }
+  }
 })
